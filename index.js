3d8b2bee827aea2b047551b0e7252e7d247a9679
mostafawanas88 <mostafawanas@hotmail.com>
2020-09-19 19:27:29 +0400

    submission with one stretch

--- index.js
+++ index.js
@@ -220,6 +220,18 @@ const gradeCalc = (mark) => {
 // Hint - you may need to study tomorrow's traning kit on arrays 
 // try looking up the .includes() method
 
+const countVowels = (str) => {
+    const vowels = ['a','e','i','o','u'];
+    let count = 0;
+    for (var i = 0; i < str.length; i++) {
+        for (var j = 0; j < vowels.length; j++) {
+            if (str[i].toLowerCase() === vowels[j]) {
+                count++
+            }
+        }
+    }
+    return count;
+}
 
 
 
